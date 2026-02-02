45df2044665a04f92aad210d3193253a60bdbc2d
VictoriaKirplyuk <Meosh99@bk.ru>
2026-02-02 20:19:01 +0300

    fast fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import { AppRouter } from "./components/AppRouter";
+import AppRouter from "./components/AppRouter";
 
 const element = document.getElementById("root");
 const root = createRoot(element);
